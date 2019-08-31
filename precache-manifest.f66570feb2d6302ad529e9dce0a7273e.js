67586a1b4b9eb53257a2edba000c0bdbd485986f
ionescuflorin <florineugen96@gmail.com>
2019-08-31 16:34:19 +0300

    Updates

---
+++ precache-manifest.f66570feb2d6302ad529e9dce0a7273e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbc6a8c993f064748a55c858bf17009d",
+    "url": "/monsters-cards/index.html"
+  },
+  {
+    "revision": "1994f9d0ce8b3db32414",
+    "url": "/monsters-cards/static/css/main.fff4db1d.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-cards/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "1994f9d0ce8b3db32414",
+    "url": "/monsters-cards/static/js/main.507efcfa.chunk.js"
+  },
+  {
+    "revision": "04488c9828952c1f9168",
+    "url": "/monsters-cards/static/js/runtime~main.e4645359.js"
+  }
+]);
